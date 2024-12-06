d88a795b520726417c9ed1c7f0691525938fac47
Sathish M <153342012+SathishTeckas@users.noreply.github.com>
2024-12-06 08:34:10 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function InvoiceDashboardPage() {
         <title>Request Invoicing</title>
       </Head>
       <div className="container m-auto  w-[100%]">
-      <Script id="chatbot" data-agent-id="67525edd86ddc08fcdd437b0" src="https://script-sepia.vercel.app/ChatBot.js"></Script>
+      <Script id="chatbot" data-agent-id="6752698d0ae893434c0e5b7f" src="https://script-sepia.vercel.app/ChatBot.js"></Script>
         <InvoiceDashboard
           config={config}
           currencies={currencies}
